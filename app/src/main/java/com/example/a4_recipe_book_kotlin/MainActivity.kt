77db0b8625ba1ab92545fd95cf006ec086a495cd
imans <imanshaterian@gmail.com>
2024-08-17 11:18:23 +0200

    Initial commit: this is the first commit, this is the foundation for the Recipe app.

---
+++ app/src/main/java/com/example/a4_recipe_book_kotlin/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.a4_recipe_book_kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a4_recipe_book_kotlin.ui.theme._4_Recipe_Book_KotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _4_Recipe_Book_KotlinTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _4_Recipe_Book_KotlinTheme {
+        Greeting("Android")
+    }
+}
